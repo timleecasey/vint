a453f484c2ac4069e270d07b34ea74ff97997ed0
Tim Casey <tlc@veeve.io>
2025-03-16 16:01:09 -0700

    Labels in check file and logging while running.

--- prob1/check.sh
+++ prob1/check.sh
@@ -1,10 +1,19 @@
 
 
+echo "RUN"
 python print_tree_2.py > log
 
+rm check_out
 
-N=10
-for i in $(seq 2 8) ; do
-    echo "N: $N M: $i"
-    grep -A${N} "N:${N} M:${i}" log | grep -v '^#' | grep -v -e '->' | sort | uniq -c
+echo "CHECK"
+for N in $(seq 5 100) ; do
+    echo "N: ${N}"
+    for i in $(seq 4 10) ; do
+        echo "N: $N M: $i" >> check_out
+        grep -A${N} "N:${N} M:${i}" log | grep -v '^#' | grep -v -e '->' | sort | uniq -c >> check_out
+    done
 done
+
+echo "Check errors"
+
+grep -v -e '--' check_out  | grep -v '^N: '
